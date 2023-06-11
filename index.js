c4a86aed21835bc381bed3f5f7263aa7817c3c81
maufdev09 <marufdev09@gmail.com>
2023-06-11 23:45:24 +0600

    payments get api sorted by date

--- index.js
+++ index.js
@@ -143,7 +143,11 @@ async function run() {
     app.get("/get-payed-classes/:email", async (req, res) => {
       const email = req.params.email;
       const query = { user: email };
-      const result = await paymentCollection.find(query).toArray();
+
+      const result = await paymentCollection
+        .find(query)
+        .sort({ date: -1 })
+        .toArray();
       res.send(result);
     });
 
